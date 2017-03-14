00fb7bc455177a909e424db2346dc3a49dbe28ed
Kamil W <df>
2017-03-14 14:07:33 +0100

    Project

--- zadanie3/zad3/Program.cs
+++ zadanie3/zad3/Program.cs
@@ -11,15 +11,17 @@ namespace zad3
         protected string imie;
         protected string nazwisko;
         protected string dataUrodzenia;
-        public Osoba(string imie_, string nazwisko_, string dataUrodzenia_)
+
+        public Osoba(string imie, string nazwisko, string dataUrodzenia)
         {
-            imie = imie_;
-            nazwisko = nazwisko_;
-            dataUrodzenia = dataUrodzenia_;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.dataUrodzenia = dataUrodzenia;
         }
+
         public void WypiszInfo()
         {
-
+            Console.WriteLine("{0} {1} {2}", imie, nazwisko, dataUrodzenia);
         }
     }
 
@@ -29,21 +31,22 @@ namespace zad3
         private string klub;
         private int liczbaGoli = 0;
 
-        public Pilkarz(string imie_, string nazwisko_, string dataUrodzenia_, string pozycja_, string klub_) : base(imie_, nazwisko_, dataUrodzenia_)
+        public Pilkarz(string imie, string nazwisko, string dataUrodzenia, string pozycja, string klub, int liczbaGoli) : base(imie, nazwisko, dataUrodzenia)
         {
-            imie = imie_;
-            nazwisko = nazwisko_;
-            dataUrodzenia = dataUrodzenia_;
-            pozycja = pozycja_;
-            klub = klub_;
+            this.pozycja = pozycja;
+            this.klub = klub;
+            this.liczbaGoli = liczbaGoli;
         }
-        public void WypiszInfo()
-        {
 
+        public new void WypiszInfo()
+        {
+            base.WypiszInfo();
+            Console.WriteLine("Pozycja: {0}, klub {1}, liczba goli: {2}", pozycja, klub, liczbaGoli);
         }
+
         public void StrzelGola()
         {
-
+            Console.WriteLine("\nGol !!! Strzelcem jest: {0} {1}\n", imie, nazwisko);
         }
     }
 
@@ -52,101 +55,102 @@ namespace zad3
         private int rok;
         private int grupa;
         private int nrIndeksu;
+        private List<Ocena> oceny;
 
-        public Student(string imie_, string nazwisko_, string dataUrodzenia_, int rok_, int grupa_, int nrIndeksu_) : base(imie_, nazwisko_, dataUrodzenia_)
+        public Student(string imie, string nazwisko, string dataUrodzenia, int rok, int grupa, int nrIndeksu) : base(imie, nazwisko, dataUrodzenia)
         {
-            imie = imie_;
-            nazwisko = nazwisko_;
-            dataUrodzenia = dataUrodzenia_;
-            rok = rok_;
-            grupa = grupa_;
-            nrIndeksu = nrIndeksu_;
+            this.rok = rok;
+            this.grupa = grupa;
+            this.nrIndeksu = nrIndeksu;
+            oceny = new List<Ocena>();
         }
 
-        public void WypiszInfo()
+        public new void WypiszInfo()
         {
-
+            base.WypiszInfo();
+            Console.WriteLine("Rok studiów: {0}, grupa: {1}, nr indeksu: {2}\n", rok, grupa, nrIndeksu);
         }
 
         public void DodajOcene(string nazwaPrzedmiotu, string data, double wartosc)
         {
-
+            oceny.Add(new Ocena(nazwaPrzedmiotu, data, wartosc));
         }
 
         public void WypiszOceny()
         {
-
+            foreach (var ocena in oceny)
+                ocena.WypiszInfo();
         }
 
         public void WypiszOceny(string nazwaPrzedmiotu)
         {
-
+            foreach (var ocena in oceny)
+                if (ocena.NazwaPrzedmiotu == nazwaPrzedmiotu)
+                    ocena.WypiszInfo();
         }
 
         public void UsunOcene(string nazwaPrzedmiotu, string data, double wartosc)
         {
+            Ocena doUsuniecia = oceny.Find(x => x.NazwaPrzedmiotu == nazwaPrzedmiotu && x.Data == data && x.Wartosc == wartosc);
 
+            if (doUsuniecia != null)
+                oceny.Remove(doUsuniecia);
         }
 
         public void UsunOceny()
         {
-
+            oceny = new List<Ocena>();
         }
 
         public void UsunOceny(string nazwaPrzedmiotu)
         {
-
+            oceny.RemoveAll(x => x.NazwaPrzedmiotu == nazwaPrzedmiotu);
         }
     }
 
     class PilkarzReczny : Pilkarz
     {
-        public Pilkarz(string imie_, string nazwisko_, string dataUrodzenia_, string pozycja_, string klub_)
+        public PilkarzReczny(string imie, string nazwisko, string dataUrodzenia, string pozycja, string klub, int liczbaGoli) : base(imie, nazwisko, dataUrodzenia, pozycja, klub, liczbaGoli)
         {
-            imie = imie_;
-            nazwisko = nazwisko_;
-            dataUrodzenia = dataUrodzenia_;
-            this.pozycja = pozycja_;
-            this.klub = klub_;
         }
-        public void StrzelGola()
-        {
 
+        public new void StrzelGola()
+        {
+            Console.WriteLine("\nGol !!! Strzelcem jest: {0} {1}\n", imie, nazwisko);
         }
     }
 
     class PilkarzNozny : Pilkarz
     {
-        public Pilkarz(string imie_, string nazwisko_, string dataUrodzenia_, string pozycja_, string klub_)
+        public PilkarzNozny(string imie, string nazwisko, string dataUrodzenia, string pozycja, string klub, int liczbaGoli) : base(imie, nazwisko, dataUrodzenia, pozycja, klub, liczbaGoli)
         {
-            imie = imie_;
-            nazwisko = nazwisko_;
-            dataUrodzenia = dataUrodzenia_;
-            this.pozycja = pozycja_;
-            this.klub = klub_;
         }
-        public void StrzelGola()
-        {
 
+        public new void StrzelGola()
+        {
+            Console.WriteLine("\nGol !!! Strzelcem jest: {0} {1}\n", imie, nazwisko);
         }
     }
 
     class Ocena
     {
+        public string NazwaPrzedmiotu { get { return nazwaPrzedmiotu; } }
+        public string Data { get { return data; } }
+        public double Wartosc { get { return wartosc; } }
         private string nazwaPrzedmiotu;
         private string data;
         private double wartosc;
 
-        public Ocena(string nazwaPrzedmiotu_, string data_, double wartosc_)
+        public Ocena(string nazwaPrzedmiotu, string data, double wartosc)
         {
-            nazwaPrzedmiotu = nazwaPrzedmiotu_;
-            data = data_;
-            wartosc = wartosc_;
+            this.nazwaPrzedmiotu = nazwaPrzedmiotu;
+            this.data = data;
+            this.wartosc = wartosc;
         }
 
         public void WypiszInfo()
         {
-
+            Console.WriteLine("Przedmiot: {0}, ocena: {1:0.0}, data: {2}", nazwaPrzedmiotu, wartosc, data);
         }
     }
 
@@ -154,6 +158,52 @@ namespace zad3
     {
         static void Main(string[] args)
         {
+            Student student = new Student("Kamil", "Lewalski", "1977/02/01", 2, 4, 122112);
+            student.DodajOcene("ZPrOb", "2017/03/12", 5.0);
+            student.DodajOcene("ZPrOb", "2017/03/14", 4.0);
+            student.DodajOcene("SR", "2017/02/10", 3.0);
+            student.DodajOcene("SR", "2017/03/09", 2.0);
+            student.DodajOcene("PSI", "2017/03/10", 5.0);
+            student.DodajOcene("SI", "2017/03/10", 4.0);
+            student.DodajOcene("AiOK", "2017/03/10", 3.0);
+
+            student.WypiszInfo();
+            student.WypiszOceny();
+
+            Console.WriteLine();
+
+            Console.WriteLine("usun ocene SR 2017/03/09 2.0");
+            student.UsunOcene("SR", "2017/03/09", 2.0);
+            Console.WriteLine("usun ocene ZPrOb 2017/03/12 5.0");
+            student.UsunOcene("ZPrOb", "2017/03/12", 5.0);
+            student.WypiszOceny();
+
+            Console.WriteLine();
+
+            Console.WriteLine("usun wszystkie oceny z ZPrOb");
+            student.UsunOceny("ZPrOb");
+            student.WypiszOceny();
+
+            Console.WriteLine();
+
+            Console.WriteLine("usun wszystkie oceny");
+            student.UsunOceny();
+            student.WypiszOceny();
+
+            Console.WriteLine("\n---------------------------\n");
+
+            Pilkarz pilkarz = new Pilkarz("Michał", "Polny", "1994/11/12", "Napastnik", "Orły", 7);
+            PilkarzReczny pilkarzReczny = new PilkarzReczny("Adam", "Góra", "1987/01/15", "Obrotowy", "Mistrzowie", 15);
+            PilkarzNozny pilkarzNozny = new PilkarzNozny("Paweł", "Pielec", "1999/12/01", "Obrońca", "Orły2", 6);
+
+            pilkarz.WypiszInfo();
+            pilkarz.StrzelGola();
+            pilkarzReczny.WypiszInfo();
+            pilkarzReczny.StrzelGola();
+            pilkarzNozny.WypiszInfo();
+            pilkarzNozny.StrzelGola();
+
+            Console.ReadKey();
         }
     }
 }
--- zadanie3/zad3/Properties/AssemblyInfo.cs
+++ zadanie3/zad3/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("zad3")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("95457ba0-0d05-4f95-bf13-a8f527e726c2")]
+[assembly: Guid("f3386e30-072c-4cc0-a43a-49e48b49cdbd")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
